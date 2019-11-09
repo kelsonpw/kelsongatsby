5bfe1714184cd9b257ac16b44090d55a31ddf87a
Kelson Warner <kelson.warner@fundingcircle.com>
2019-11-09 12:50:17 -0800

    dep change

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,6 @@ module.exports = {
 	plugins: [
 		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-styled-components',
-		'gatsby-transformer-sharp',
-		'gatsby-plugin-sharp',
 		{
 			resolve: 'gatsby-source-graphql',
 			options: {
